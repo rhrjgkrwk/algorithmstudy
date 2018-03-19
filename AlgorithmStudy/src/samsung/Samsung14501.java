21e12ca28d43a27df1ea89e93bea6fe94dc9ffe7
ChangheeLee <rhrjgkrwk@gmail.com>
2018-03-19 14:29:02 +0900

    삼성 14501 퇴사 문제

--- AlgorithmStudy/src/samsung/Samsung14501.java
+++ AlgorithmStudy/src/samsung/Samsung14501.java
@@ -10,18 +10,30 @@ public class Samsung14501 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		schedule = new int[N][2]; //0 : 일수 1 : 페이 
-		d = new int[N];
-		for (int i = 0; i < N; i++) {
+		schedule = new int[N+1][2]; //0 : 일수 1 : 페이 
+		//startDate = i
+		//endDate = startDate(i) + [i][0] -1
+		
+		d = new int[N+1];
+		for (int i = 1; i <= N; i++) {
 			for (int j = 0; j < 2; j++) {
 				schedule[i][j] = sc.nextInt();
 			}
 		}
+		dp();
+		System.out.println(d[N]);
 		sc.close();
 	}
 	public static void dp() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j <= i; j++) {
+		if (schedule[1][0]==1) {
+			d[1]=schedule[1][1];
+		}
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				int endDate = j + schedule[j][0] -1;
+				if (endDate <= i && d[i] < d[j-1]+schedule[j][1]) {
+					d[i] = d[j-1]+schedule[j][1];
+				}
 			}
 		}
 	}
